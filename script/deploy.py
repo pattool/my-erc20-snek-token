868ef1d1acd3c16c1d4106bf85acc434aff63b32
Patrick Pekel <pekelpatrick@gmail.com>
2026-02-19 19:03:15 +0100

    Small  update deploy file

--- script/deploy.py
+++ script/deploy.py
@@ -11,22 +11,16 @@ INITIAL_SUPPLY = to_wei(1000, "ether") # or int(1000e18) gives 18 decimals to 10
 def deploy() -> VyperContract:
     snek_contract = snek_token.deploy(INITIAL_SUPPLY)
     print(f"\nDeployed Snektoken at {snek_contract.address}")
-
-   #active_network = get_active_network()
-   #if active_network.has_explorer() and active_network.is_local_or_forked_network() is False:
-   #    result = active_network.moccasin_verify(snek_contract)
-   #    result.wait_for_verification()
-
     
     active_network = get_active_network()
     print(f"\nActive network: {active_network.name}")
     print(f"Explorer URI: {active_network.explorer_uri}")
     print(f"Explorer type: {active_network.explorer_type}")
-    print(f"Network has explorer: {active_network.has_explorer()}\n")
+    print(f"Network has explorer: {active_network.has_explorer}\n")
     
 
     if active_network.has_explorer() and active_network.is_local_or_forked_network() is False:
-        print(f"Waiting 20 seconds for contract to be indexed by {active_network.explorer_type} ...")
+        print(f"Waiting 20 seconds for contract to be indexed by {active_network.explorer_type} ...\n")
         time.sleep(20)
 
         print(f"Starting {active_network.explorer_type} verification...")
@@ -34,7 +28,7 @@ def deploy() -> VyperContract:
             result = active_network.moccasin_verify(snek_contract)
             print(f"Verification request submitted to {active_network.explorer_type}")
             result.wait_for_verification()
-            print("Verification completed succesfully!")
+            print("Verification completed succesfully!\n")
         except Exception as e:
             print(f"Verification failed with error: {e}")
             print(f"Error type: {type(e).__name__}")
